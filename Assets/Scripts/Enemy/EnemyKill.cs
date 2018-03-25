464826572da5d90af3023faa50342d44802def61
Callum Smith <techdragon@tuta.io>
2018-03-25 16:45:04 +0100

    Added main menu and started terrain

---
+++ Assets/Scripts/Enemy/EnemyKill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyKill : MonoBehaviour
+{
+    private static bool dead = false;
+    
+    void Update()
+    {
+        if (dead) //if the player is dead; load the main menu scene
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        if (transform.position.y <= -25) //if the player falls out of the world; load main menu scene
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+}
