3fd6cb63daaa0a9e7f4d5551bc293453a85be00e
Callum Smith <techdragon@tuta.io>
2018-05-03 22:44:51 +0100

    Upgraded to Unity 2018.1

--- Assets/Scripts/Enemy/EnemyKill.cs
+++ Assets/Scripts/Enemy/EnemyKill.cs
@@ -3,21 +3,40 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class EnemyKill : MonoBehaviour
 {
-    private static bool dead = false;
+    private bool dead = false;
     
     void Update()
     {
         if (dead) //if the player is dead; load the main menu scene
         {
-            SceneManager.LoadScene("Menu");
+            OnDeath();
         }
         if (transform.position.y <= -25) //if the player falls out of the world; load main menu scene
         {
-            SceneManager.LoadScene("Menu");
+            OnDeath();
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Enemy")
+        {
+            OnDeath();
         }
     }
 
+    private void OnDeath()
+    {
+        SceneManager.LoadScene("Menu");
+        MouseLook.lockCursor = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Debug.Log(MouseLook.lockCursor);
+        
+    }
 }
